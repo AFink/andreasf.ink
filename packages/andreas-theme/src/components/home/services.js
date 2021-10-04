2aca7997eb48133a5cd91280dcb7fd9a5ccdf988
Andreas Fink <mail@afink.dev>
2021-10-04 22:58:05 +0200

    Make boxes the same height in one row

--- packages/andreas-theme/src/components/home/services.js
+++ packages/andreas-theme/src/components/home/services.js
@@ -34,12 +34,12 @@ const Services = ({ state, libraries  }) => {
     return (
         <div id="services" className="bg-light">
             <div className="container col-xxl-8 px-4 py-5 text-white-50 text-center">
-                <div className="row align-items-center g-5 py-5 justify-content-md-center">
+                <div className="row display-flex align-items-center align-items-md-stretch g-5 py-5 justify-content-md-center">
                     <h2 className="display-6 fw-bold lh-1 mb-3 text-start text-body">Services</h2>
                     {services.map(s => {
                         return (
                             <div className="col col-md-4">
-                                <div className="service rounded p-4  bg-primary">
+                                <div className="service rounded p-4 bg-primary h-100">
                                     <div class="service-icon bg-info">
                                         <svg class="bi" width="1em" height="1em" viewBox="0 0 16 16">
                                             <path d="M2.5 3.5a.5.5 0 0 1 0-1h11a.5.5 0 0 1 0 1h-11zm2-2a.5.5 0 0 1 0-1h7a.5.5 0 0 1 0 1h-7zM0 13a1.5 1.5 0 0 0 1.5 1.5h13A1.5 1.5 0 0 0 16 13V6a1.5 1.5 0 0 0-1.5-1.5h-13A1.5 1.5 0 0 0 0 6v7zm1.5.5A.5.5 0 0 1 1 13V6a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-.5.5h-13z"></path>
