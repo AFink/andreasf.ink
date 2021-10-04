1b424007bcbaa115525fe9bbc388e54de34fc95c
Andreas Fink <mail@afink.dev>
2021-10-04 22:12:11 +0200

    Add services

--- packages/andreas-theme/src/components/home/home.js
+++ packages/andreas-theme/src/components/home/home.js
@@ -1,10 +1,12 @@
 import { connect, styled } from "frontity";
 import Header from "./header";
+import Services from "./services";
 
 const Home = ({ state, actions }) => {
     return (
         <div id="home">
             <Header />
+            <Services />
         </div>
     );
 };
