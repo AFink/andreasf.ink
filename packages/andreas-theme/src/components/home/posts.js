2a18473b34d6c5e9b288b20ba6e18c4fa8e0748c
Andreas Fink <mail@afink.dev>
2021-10-10 18:32:18 +0200

    Rewrite posts

--- packages/andreas-theme/src/components/home/posts.js
+++ packages/andreas-theme/src/components/home/posts.js
@@ -1,55 +1,30 @@
-import { connect, styled } from "frontity";
-import Link from "@frontity/components/link";
+import { connect } from "frontity";
+
 import Container from 'react-bootstrap/Container';
-import { jsUcfirst } from "shufflejs-react/lib/Utils";
+import Row from "react-bootstrap/Row";
 
-import moment from 'moment';
+import { jsUcfirst } from "@finki70/shufflejs-react/lib/Utils";
+import Item from "../list/list-item";
 
-const Posts = ({ state, libraries }) => {
-    const Html2React = libraries.html2react.Component;
-
+const Posts = ({ state }) => {
     const po = state.source.data["three-posts/"];
 
     const { items } = state.source.data["all-categories/"];
     const posts = po.items.map(p => {
-        const media = state.source.attachment[p.featured_media];
-
-        p.mediaHelper = "";
-
-        if (media !== undefined) {
-            if ("source_url" in media) {
-                p.mediaHelper = media.source_url;
-            }
-        }
-
         p.categoryHelper = p.categories.map(c => jsUcfirst(items.find(i => i.id == c) ? items.find(i => i.id == c).name : ''));
         return p;
     })
 
     return (
         <div id="posts" className="bg-rosa">
-            <div className="container col-xxl-8 px-4 py-5 ">
-                <div className="row align-items-center g-5 py-5">
-                    <h2 className="display-6 fw-bold lh-1 mb-3 text-start text-body">Posts</h2>
-                    {posts.map(p => {
-                        console.log(p)
-                        return (
-                            <div className="col col-md-4">
-                                <Link link={p.link} className="post rounded text-decoration-none text-white-50 text-center hvr-float w-100">
-                                    <div class="post-image">
-                                        <img src={p.mediaHelper} />
-                                    </div>
-                                    <div className="details p-4 bg-primary">
-                                        <h2 className="text-light">{p.title.rendered}</h2>
-                                        <div className="excerpt"><Html2React html={p.excerpt.rendered} /></div>
-                                        <span>{moment(p.date).format('LL')}</span>
-                                    </div>
-                                </Link>
-                            </div>
-                        );
+            <Container className="col-xxl-8 px-4 py-5 ">
+                <Row className="align-items-center g-5 py-5 align-items-md-stretch">
+                    <h2 className="display-6 fw-bold lh-1 mb-3 text-body text-center text-md-start">{state.lang[state.currentLang].home.posts.title}</h2>
+                    {posts.map(item => {
+                        return <Item key={item.id} item={item} />;
                     })}
-                </div>
-            </div >
+                </Row>
+            </Container >
         </div>
     )
 };
--- packages/andreas-theme/src/components/list/index.js
+++ packages/andreas-theme/src/components/list/index.js
@@ -1,7 +1,3 @@
 import { loadable } from "frontity";
 
-/**
- * Codesplit the list component so it's not included if the users
- * load a post directly.
- */
 export default loadable(() => import("./list"));
