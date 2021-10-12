61c9c7cc19abfb3bccbf5bf2a02336e024dd0ba7
Andreas Fink <mail@afink.dev>
2021-10-12 20:46:19 +0200

    Modify projects

--- packages/andreas-theme/src/components/home/projects.js
+++ packages/andreas-theme/src/components/home/projects.js
@@ -1,12 +1,12 @@
-import { connect, styled } from "frontity";
+import { connect } from "frontity";
 import Link from "@frontity/components/link";
+
 import Container from 'react-bootstrap/Container';
-import { Col, Row } from "react-bootstrap";
+import Row from "react-bootstrap/Row";
 
-import { SortFilter } from "shufflejs-react";
-import { jsUcfirst } from "shufflejs-react/lib/Utils";
+import { SortFilter } from "@finki70/shufflejs-react";
+import { jsUcfirst } from "@finki70/shufflejs-react/lib/Utils";
 
-import moment from 'moment';
 
 const Projects = ({ state, actions }) => {
     const data = state.source.get("/projects/");
@@ -15,17 +15,11 @@ const Projects = ({ state, actions }) => {
     ) ?? [];
 
     const { items } = state.source.data["all-categories/"];
-    const cats = {
-        "Body Types": {
-        },
-    }
-    items.map(c => {
-        cats['Body Types'][String(c.name)] = {
-            slug: c.slug,
-        };
-    });
+
+    const usedcats = [];
 
     projects.map(p => {
+
         const media = state.source.attachment[p.featured_media];
 
         p.mediaHelper = "";
@@ -36,35 +30,54 @@ const Projects = ({ state, actions }) => {
             }
         }
 
-        p.categoryHelper = p.categories.map(c => jsUcfirst(items.find(i => i.id == c).name));
+        p.categoryHelper = p.categories.map(c => {
+            const cat = items.find(i => i.id == c) ?? null;
+
+            if (cat == null) { return null };
+
+            usedcats.push(cat.id);
+
+            return jsUcfirst(cat.name);
+        });
         return p;
     })
-    //projects.map(p => console.log(p.categoryHelper));
 
-    const defaultSort = ""; // empty string defaults to show all
+    const cats = {
+        "Body Types": {
+        },
+    }
+    console.log([...new Set(usedcats)]);
+
+    items.filter(c => [...new Set(usedcats)].includes(c.id)).map(c => {
+        cats['Body Types'][String(c.name)] = {
+            slug: c.slug,
+        };
+    });
+
+    const defaultSort = "";
 
     const itemSelector = "sortblock";
     if (projects.length == 0) {
         return (
-            <div>
-                uwu
-            </div>
+            <>
+            </>
         );
     }
 
     return (
-        <div className="container col-xxl-8 px-4 py-5">
-            <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
-                <h2 className="display-6 fw-bold lh-1 mb-3">Projects</h2>
+        <Container className="col-xxl-8 px-4 py-5">
+            <Row className="flex-lg-row-reverse align-items-center g-5 py-5">
+                <h2 className="display-6 fw-bold lh-1 mb-3 text-center text-md-start">{state.lang[state.currentLang].home.projects.title}</h2>
                 <SortFilter
                     defaultSort={defaultSort}
                     taxonomies={cats}
                     itemSelector={itemSelector}
+                    sortAllText={state.lang[state.currentLang].home.projects.all}
                 >
                     <Elements records={projects} itemSelector={itemSelector} />
                 </SortFilter>
-            </div>
-        </div>
+            </Row>
+        </Container>
     );
 };
 
@@ -78,33 +91,23 @@ function Element(props) {
     const { itemSelector, record } = props;
     return (
         <div
+            key={record.id}
             className={`col col-md-3 ${itemSelector}`}
             data-groups={`${JSON.stringify(record.categoryHelper.map(s => String(s)))}`}>
-
-            <Link link={record.link} className="text-decoration-none">
-                <div style={{ 'background-image': `url("${record.mediaHelper}")` }} className="rounded bg-primary project">
-                    <div className="details">
-                        <h3 className="pt-5 mt-4 mb-3 lh-1">
-                            {record.title.rendered}
-                        </h3>
-
-                        {/*<div class="item-meta white is-primary-tag">
-                    <time datetime={record.date.substr(0, 10)}>{moment(record.date).fromNow()}</time>
-                </div>*/}
-
-                        {record.categoryHelper.map(s => {
-                            return (
-                                <>
-                                    <span class="" href="">{s}</span>{' '}
-                                </>
-                            );
-                        })}
+            <Link link={record.link} className="text-decoration-none w-100">
+                <div style={{ 'backgroundImage': `url("${record.mediaHelper}")` }} className="rounded bg-primary project d-flex">
+                    <div className="details rounded d-flex flex-column justify-content-end w-100">
+                        <h3 className="pt-5 mt-4 mb-3 lh-1" dangerouslySetInnerHTML={{ __html: record.title.rendered }}></h3>
+                        <ul class="list-inline list-inline-dots mb-0">
+                            {record.categoryHelper.map(s => {
+                                return (
+                                    <li class="list-inline-item">{s}</li>
+                                );
+                            })}
+                        </ul>
                     </div>
                 </div>
             </Link>
-
-
-
         </div>
     );
 }
